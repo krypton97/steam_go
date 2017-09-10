3b8574bf27a28b55e8ff5e2802f00025bb6aeedf
krypton97 <bldcuber@gmail.com>
2017-09-10 15:27:18 +0300

    Update player.go

--- player.go
+++ player.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	
+	"github.com/valyala/fasthttp"
 )
 
 type PlayerSummaries struct {
@@ -33,15 +34,10 @@ type PlayerSummaries struct {
 
 func GetPlayerSummaries(steamId, apiKey string) (*PlayerSummaries, error) {
 	url := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s", apiKey, steamId)
-	resp, err := http.Get(url)
+	_, body, err := fasthttp.Get(nil, url)
 	if err != nil {
 		return nil, err
 	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
 	type Result struct {
 		Response struct {
 			Players []PlayerSummaries `json:"players"`
