6840cd293e59486698b57edeb0ff1348f2e6d012
krypton97 <bldcuber@gmail.com>
2017-09-11 19:33:28 +0300

    Update auth.go
    upgraded to fasthttp

--- auth.go
+++ auth.go
@@ -66,51 +66,46 @@ func (id OpenId) AuthUrl() string {
 	return url
 }
 
-func (id *OpenId) ValidateAndGetId() (string, error) {
+func (id *OpenId) ValidateAndGetId() ([]byte, error) {
 	if !bytes.Equal(id.Mode(), []byte("id_res") ) {
-		return "", errors.New("Mode must equal to \"id_res\".")
+		return []byte{}, errors.New("Mode must equal to \"id_res\".")
 	}
 
 	if !bytes.Equal(id.data.Peek("openid.return_to"), []byte(id.returnUrl)) {
-		return "", errors.New("The \"return_to url\" must match the url of current request.")
+		return []byte{}, errors.New("The \"return_to url\" must match the url of current request.")
 	}
-
-	params := make(url.Values)
-	params.Set("openid.assoc_handle", string(id.data.Peek("openid.assoc_handle")))
-	params.Set("openid.signed", string(id.data.Peek("openid.signed")))
-	params.Set("openid.sig", string(id.data.Peek("openid.sig")))
-	params.Set("openid.ns", string(id.data.Peek("openid.ns")))
-
-	split := strings.Split(string(id.data.Peek("openid.signed")), ",")
+	
+	params := fasthttp.AcquireArgs()
+	params.AddBytesV("openid.assoc_handle", id.data.Peek("openid.assoc_handle"))
+	params.AddBytesV("openid.signed", id.data.Peek("openid.signed"))
+	params.AddBytesV("openid.sig", id.data.Peek("openid.sig"))
+	params.AddBytesV("openid.ns", id.data.Peek("openid.ns"))
+	
+	split := bytes.Split(id.data.Peek("openid.signed"), []byte{','})
 	for _, item := range split {
-		params.Set("openid."+item, string(id.data.Peek("openid."+item)))
+		params.AddBytesV("openid."+ string(item), id.data.Peek("openid."+string(item)))
 	}
-	params.Set("openid.mode", "check_authentication")
+	params.AddBytesV("openid.mode", []byte("check_authentication"))
 
-	resp, err := http.PostForm(steam_login, params)
+	_, content, err := fasthttp.Post(nil, steam_login, params)
 	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
+		return []byte{}, err
 	}
 
-	response := strings.Split(string(content), "\n")
-	if response[0] != "ns:"+openId_ns {
-		return "", errors.New("Wrong ns in the response.")
+	response := bytes.Split(content, []byte{'\n'})
+	if !bytes.Equal(response[0], []byte("ns:" + openId_ns)) {
+		return []byte{}, errors.New("Wrong ns in the response.")
 	}
-	if strings.HasSuffix(response[1], "false") {
-		return "", errors.New("Unable validate openId.")
-	}
-
-	openIdUrl := string(id.data.Peek("openid.claimed_id"))
-	if !validation_regexp.MatchString(openIdUrl) {
-		return "", errors.New("Invalid steam id pattern.")
+	if bytes.HasSuffix(response[1], []byte("false")) {
+		return []byte{}, errors.New("Unable validate openId.")
 	}
 
-	return digits_extraction_regexp.ReplaceAllString(openIdUrl, ""), nil
+	openIdUrl := id.data.Peek("openid.claimed_id")
+	if !validation_regexp.Match(openIdUrl) {
+		return []byte{}, errors.New("Invalid steam id pattern.")
+	}
+
+	return digits_extraction_regexp.ReplaceAll(openIdUrl, []byte("")), nil
 }
 
 func (id OpenId) ValidateAndGetUser(apiKey string) (*PlayerSummaries, error) {
@@ -118,7 +113,7 @@ func (id OpenId) ValidateAndGetUser(apiKey string) (*PlayerSummaries, error) {
 	if err != nil {
 		return nil, err
 	}
-	return GetPlayerSummaries(steamId, apiKey)
+	return GetPlayerSummaries(string(steamId), apiKey)
 }
 
 func (id OpenId) Mode() []byte {
